d1e03e519aa5d200567cf3bde4df86645d99e0c3
Vanessa Su <vnessa.su@gmail.com>
2021-06-16 14:34:11 -0400

    :hammer: Add GroupSelection model
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

---
+++ StudentRandomizerMvc/Models/GroupSelection.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentRandomizerMvc.Models
+{
+  public class GroupSelection
+  {
+    public static List<Group> SelectBestGroups(List<Group> allGroups, int numberOfGroups, List<Student> allStudents)
+    {
+      List<Group> sortedGroupsByScore = allGroups.OrderBy(group => group.GroupScore).ToList();
+      List<Group> selectedGroups = new List<Group>();
+      List<Student> studentsInSelectedGroups = new List<Student>();
+
+      for (int index = 0; index < sortedGroupsByScore.Count; index++)
+      {
+        Group currentGroup = sortedGroupsByScore[index];
+        List<Student> currentGroupStudents = currentGroup.DevTeamStudents;
+        List<Student> commonStudents = studentsInSelectedGroups.Intersect(currentGroupStudents).ToList();
+
+        if (commonStudents.Count == 0)
+        {
+          selectedGroups.Add(currentGroup);
+          studentsInSelectedGroups.AddRange(currentGroupStudents);
+        }
+
+        if (selectedGroups.Count == numberOfGroups)
+        {
+          break;
+        }
+      }
+
+      List<Student> leftoverStudents = Student.FindStudentsNotIncluded(allStudents, studentsInSelectedGroups);
+      List<Group> leftoversAddedGroups = AddExtraStudents(selectedGroups, leftoverStudents);
+      return leftoversAddedGroups;
+    }
+
+    public static List<Group> AddExtraStudents(List<Group> groups, List<Student> leftoverStudents)
+    {
+      int groupCount = groups.Count;
+      int groupIndex = 0;
+
+      foreach (Student student in leftoverStudents)
+      {
+        groups[groupIndex].DevTeamStudents.Add(student);
+        groupIndex++;
+        if (groupIndex >= groupCount)
+        {
+          groupIndex = 0;
+        }
+      }
+
+      return groups;
+    }
+  }
+}
